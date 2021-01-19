141812b18132898a5d4dfe03070a7446fdcc2d3a
dearhyunmo <33799907+dearhyunmo@users.noreply.github.com>
2021-01-19 14:49:37 +0900

    Create Leeting.js

---
+++ FrontEnd/src/routes/Leeting.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Slider from "react-slick";
+import "./Slider.css";
+import "./Home.css";
+
+class Leeting extends React.Component {
+    render() {
+        return (
+            <div id="main_content">
+                
+            </div>
+        )
+    }
+}
+  
+export default Leeting;
